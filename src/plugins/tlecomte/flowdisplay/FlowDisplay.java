bed51427ce679bc3d31119f06fa785e7bd6a2e35
Timothée Lecomte <timothee.lecomte@pasteur.fr>
2012-05-30 15:14:25 +0200

    Add dummy classes that extend Plugin to satisfy Icy Plugin system.

---
+++ src/plugins/tlecomte/flowdisplay/FlowDisplay.java
@@ -0,0 +1,8 @@
+package plugins.tlecomte.flowdisplay;
+
+import icy.plugin.abstract_.Plugin;
+import icy.plugin.interface_.PluginLibrary;
+
+public class FlowDisplay  extends Plugin implements PluginLibrary {
+
+}
---
+++ src/plugins/tlecomte/rectangularFEM/RectangularFEM.java
@@ -0,0 +1,8 @@
+package plugins.tlecomte.rectangularFEM;
+
+import icy.plugin.abstract_.Plugin;
+import icy.plugin.interface_.PluginLibrary;
+
+public class RectangularFEM extends Plugin implements PluginLibrary {
+
+}
